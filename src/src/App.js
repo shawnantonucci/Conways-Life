120ccb08bd868784fcab9df7329731ddb7736082
shawnantonucci <shawnantonucci2018@gmail.com>
2019-07-17 15:51:05 -0700

    algorithm finished

--- src/src/App.js
+++ src/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
+import { useInterval } from "./hooks/useInterval";
 
 import DisplayGrid from "./components/Grid/Grid";
 
@@ -8,12 +9,16 @@ let running = false;
 function App() {
   // ============== Hooks ============== //
   const [grid, setGrid] = useState([]);
-  const [changeState, setChangeState] = useState(false); // forces change state, grid is too nested to trigger a change
-  const [generation, setGenerations] = useState(-1);
-  // const [running, setRunning] = useState(false);
+  const [changeState, setChangeState] = useState(false);
+  const [generation, setGeneration] = useState(-1);
+  const [interval, setInterval] = useState(null);
+  const [speed, setSpeed] = useState(100);
 
   const refGeneration = useRef();
   refGeneration.current = generation;
+
+  const refGrid = useRef();
+  refGrid.current = grid;
 
   // ============== Create empty grid ============== //
   useEffect(() => {
@@ -27,7 +32,7 @@ function App() {
     const row = e.target.dataset.row;
     const col = e.target.dataset.col;
     let newGrid = grid;
-    newGrid[row][col] == 0 ? (newGrid[row][col] = 1) : (newGrid[row][col] = 0);
+    newGrid[row][col] === 0 ? (newGrid[row][col] = 1) : (newGrid[row][col] = 0);
     // console.log("CELL: ", newGrid[row][col])
     setGrid(newGrid);
     // console.log(grid)
@@ -50,22 +55,21 @@ function App() {
 
   // ============== Algorithm for conway's rules of life ============== //
   // ===== Start Game ===== //
-  let timerId;
-
-  const runGame = e => {
-    running = true;
-    timerId = requestAnimationFrame(rulesOfLife);
+  const playButton = () => {
+    setInterval(speed);
   };
 
   // ===== Stop Game ===== //
-  const stopGameOfLife = () => {
-    running = false;
-    cancelAnimationFrame(timerId);
-    console.log("Stopped");
+  const pauseButton = () => {
+    setInterval(null);
   };
 
+  useInterval(() => {
+    rulesOfLife();
+  }, interval);
+
   const checkGeneration = () => {
-    setGenerations(refGeneration.current + 1);
+    setGeneration(refGeneration.current + 1);
   };
 
   useEffect(() => {
@@ -74,27 +78,33 @@ function App() {
 
   // ===== Algorithm for game ===== //
   const rulesOfLife = () => {
-    // rule1: any live cell with fewer than two live neighbors dies, as if by underpopulation.
-    // rule2: any live cell with two or three live neighbors lives on to the next generation.
-    // rule3: any live cell with more than three live neighbors dies, as if by overpopulation
-    // rule4: any dead cell with three live neighbors becomes a live cell, as if by reproduction.
-    if (running) {
-      console.log("Running");
-      checkGeneration();
+    let tempArr = emptyGrid();
+    checkGeneration();
+    console.log("Running");
 
+    if (!running) {
       for (let row = 0; row < grid.length; row++) {
-        // setGenerations(generation + 1);
         // column loop
         for (let col = 0; col < grid[row].length; col++) {
           let count = numNeighbors(row, col);
-          console.log("Row: ", row, "Col: ", col);
+
+          // cell logic
+          if (count < 2) {
+            tempArr[row][col] = 0;
+          } else if (count === 2) {
+            tempArr[row][col] = grid[row][col];
+          } else if (count === 3) {
+            tempArr[row][col] = 1;
+          } else {
+            tempArr[row][col] = 0;
+          }
         }
       }
-      requestAnimationFrame(rulesOfLife);
-    } else {
-      console.log("Not Running");
-    }
-    if (running == false) {
+      setGrid(tempArr);
+      console.log(running);
+      if (running) {
+        return null;
+      }
     }
   };
 
@@ -109,7 +119,7 @@ function App() {
       count += grid[row - 1][col];
     }
     // top right
-    if (row !== 0 && col === grid.length) {
+    if (row !== 0 && col !== grid.length) {
       count += grid[row - 1][col + 1];
     }
     // left
@@ -140,11 +150,10 @@ function App() {
       <div className="App-header">
         <DisplayGrid grid={grid} setGrid={setGrid} changeCell={changeCell} />
         <div>
-          <button onClick={runGame}>Start!</button>
-          <button onClick={stopGameOfLife}>STOP!!!</button>
+          <button onClick={playButton}>Start</button>
+          <button onClick={pauseButton}>Pause</button>
         </div>
         <h2>Generations: {generation}</h2>
-        {/* TODO: Make reset button */}
       </div>
     </div>
   );
--- src/src/components/Grid/Grid.js
+++ src/src/components/Grid/Grid.js
@@ -1,17 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Grid.css";
 
 function DisplayGrid({ grid, setGrid, changeCell }) {
-  // function changeCell(e) {
-  //     e.preventDefault()
-  //     const row = e.target.dataset.row;
-  //     const col = e.target.dataset.col;
-  //     let newGrid = grid;
-  //     newGrid[row][col] == 0 ? newGrid[row][col] = 1 : newGrid[row][col] = 0
-  //     console.log("CELL: ", newGrid[row][col])
-  //     setGrid(newGrid)
-  // }
-
   return (
     <div className="Grid-container">
       {grid.map((row, rowIndex) => {
