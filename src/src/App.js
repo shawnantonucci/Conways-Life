562a619dbb7d7c872ccd9898a89cf493ea6bf8ad
shawnantonucci <shawnantonucci2018@gmail.com>
2019-07-16 19:16:24 -0700

    algorithm wip

--- src/src/App.js
+++ src/src/App.js
@@ -4,6 +4,9 @@ import Grid from "./components/Grid/Grid";
 
 function App() {
   const [grid, setGrid] = useState([]);
+  const [changeState, setChangeState] = useState(false);
+  const [stop, setStop] = useState(false);
+  const [generation, setGeneration] = useState(0);
 
   // Create rows and columns with zeros
   useEffect(() => {
@@ -14,16 +17,77 @@ function App() {
         testArr[i][j] = 0;
       }
     }
-    // console.log(testArr, "From testArray");
     setGrid(testArr);
   }, []);
-  // console.log(grid, "From useState");
+
+  let timerId;
+
+  const startGame = e => {
+    e.preventDefault();
+    timerId = setInterval(gameOfLife, 500);
+  };
+
+  const stopGame = e => {
+    e.preventDefault();
+    clearInterval(timerId);
+    console.log("stopped");
+  };
+
+  const gameOfLife = () => {
+    const newGrid = [];
+    const length = grid.length;
+    for (let row = 0; row < length; row++) {
+      newGrid.push([]);
+      for (let col = 0; col < length; col++) {
+        const neighbors = getNeighbors(grid, row, col);
+        if (neighbors === 3 && !grid[row][col]) {
+          newGrid[row][col] = 1;
+        } else if ((neighbors === 2 || neighbors === 3) && grid[row][col]) {
+          newGrid[row][col] = 1;
+        } else {
+          newGrid[row][col] = 0;
+        }
+      }
+    }
+    setGrid(newGrid);
+    setGeneration(generation + 1);
+  };
+
+  const getNeighbors = (board, i, j) => {
+    const length = board.length;
+    let count = 0;
+    for (let x = -1; x <= 1; x++) {
+      for (let y = -1; y <= 1; y++) {
+        if (!x && !y) {
+          continue;
+        }
+        const ix = i + x;
+        const jy = j + y;
+        if (ix >= 0 && ix < length && jy >= 0 && jy < length) {
+          if (board[ix][jy]) {
+            count++;
+          }
+        }
+      }
+    }
+    return count;
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>Game Of Life</h2>
-        <Grid grid={grid} setGrid={setGrid} />
+        <p>{generation}</p>
+        <Grid
+          grid={grid}
+          setGrid={setGrid}
+          changeState={changeState}
+          setChangeState={setChangeState}
+        />
+        <div>
+          <button onClick={e => startGame(e)}>Start!</button>
+          <button onClick={e => stopGame(e)}>Stop!</button>
+        </div>
       </header>
     </div>
   );
--- src/src/components/Grid/Grid.js
+++ src/src/components/Grid/Grid.js
@@ -1,20 +1,44 @@
 import React, { useState } from "react";
 import "./Grid.css";
 
-const Grid = ({ grid, setGrid }) => {
+const Grid = ({ grid, setGrid, changeState, setChangeState }) => {
+  const changeCell = e => {
+    e.preventDefault();
+    let col = e.target.dataset.col;
+    let row = e.target.dataset.row;
+    // console.log(col, "From col");
+    // console.log(row, "From row");
+    let newGrid = grid;
+    // newGrid[row][col];
+    console.log(newGrid[row][col], "Old Grid");
+    newGrid[row][col] == 0 ? (newGrid[row][col] = 1) : (newGrid[row][col] = 0);
+    console.log(newGrid[row][col], "New Grid");
+    setGrid(newGrid);
+    if (changeState === false) {
+      setChangeState(true);
+    } else {
+      setChangeState(false);
+    }
+  };
+
   return (
     <div className="Grid-container">
-      {grid.map(row => {
+      {grid.map((row, rowIndex) => {
         return (
-          <div className="Grid-row">
-            {row.map(col => {
+          <div key={`${rowIndex}`} className="Grid-row">
+            {row.map((col, colIndex) => {
               return (
-                <div className={`Grid-column ${col === 1 ? "alive" : null}`} />
+                <div
+                  onClick={e => changeCell(e)}
+                  className={`Grid-column ${col === 1 ? "alive" : null}`}
+                  data-col={`${colIndex}`}
+                  data-row={`${rowIndex}`}
+                  key={`${colIndex}`}
+                />
               );
             })}
           </div>
         );
-        return <div className="Grid-column" />;
       })}
     </div>
   );
